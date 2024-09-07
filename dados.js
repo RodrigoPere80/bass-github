09c719ede5b4598dd9381e15db53d94963a5f068
RodrigoPere80 <equipecapux@gmail.com>
2024-09-07 18:59:09 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Fender Jazz Bass",
+        descricao: "O Fender Jazz Bass (ou J-Bass) foi o segundo modelo de contra-baixos elétricos criado por Leo Fender. O Jazz Bass tem um som mais articulado e definido do que o seu antecessor, o Fender Precision Bass. ",
+        link: "https://pt.wikipedia.org/wiki/Fender_Jazz_Bass",
+        tags: "Fender baixo Jazz"
+    },
+    {
+        titulo: "Giannini Masterwood",
+        descricao: "A Giannini possui tradição tamanha no mercado de contrabaixos que muitos colocam nossos produtos dos anos 70 lado a lado com os instrumentos mais celebrados do planeta.",
+        link: "https://www.giannini.com.br/produtos/contra-baixo-eletrico-4-cordas-giannini-masterwood-sonic-st-bass-gmw83b-5-pau-ferro/",
+        tags: "Giannini contrabaixo"
+    },
+    {
+        titulo: "Ibanez GSR180-BK",
+        descricao: "4 cordas é uma escolha excepcional para baixistas que buscam qualidade e versatilidade. Com um braço em maple e escala em pau-ferro, este baixo oferece uma tocabilidade suave e confortável.",
+        link: "https://www.x5music.com.br/baixo-ibanez-gsr180-bk-4-cordas/p",
+        tags: "4 cordas baixista"
+    }
+];
