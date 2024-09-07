75593ec1277c812a5b1858c8e6f46a589cd0546f
RodrigoPere80 <equipecapux@gmail.com>
2024-09-07 19:04:49 -0300

    Incluindo novo contrabaixo

--- dados.js
+++ dados.js
@@ -16,5 +16,11 @@ let dados = [
         descricao: "4 cordas é uma escolha excepcional para baixistas que buscam qualidade e versatilidade. Com um braço em maple e escala em pau-ferro, este baixo oferece uma tocabilidade suave e confortável.",
         link: "https://www.x5music.com.br/baixo-ibanez-gsr180-bk-4-cordas/p",
         tags: "4 cordas baixista"
-    }
+    },
+    {
+        titulo: "Ibanez GSR180-BK",
+        descricao: "O Concert Bass JS2 de quatro cordas e escala de 34" apresenta um corpo de Poplar leve e ressonante, braço de Maple parafusado com reforço de grafite, e escala de Amaranth com raio composto de 12"-16", 24 trastes jumbo e marcações de dente de tubarão em madrepérola.",
+        link: "https://www.megadisconildo.com.br/baixo-jackson-4c-js2-concert-bass-satin-black",
+        tags: "5 cordas baixista"
+    },
 ];
